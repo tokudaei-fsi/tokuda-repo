b86be52a97a260c83678c1bd3925d7ea005cd8c8
tokudaei-fsi <51919923+tokudaei-fsi@users.noreply.github.com>
2019-07-12 23:22:12 +0900

    Update backend.tf

--- TerraForm/remote01/backend.tf
+++ TerraForm/remote01/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "ap-northeast-1"
     bucket         = "s3-fsi-terraform-state"
-    key            = "tfstate/common/sg.tfstate"
+    key            = "tfstate/common/sg2.tfstate"
     dynamodb_table = "fsi-tfstate-lock"
   }
 }
